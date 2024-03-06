8efe720601b6356cf85c83346a1bdb42f6a7bcbc
Murillo Alves Couto <murilloacouto@outlook.com>
2024-03-06 17:28:32 -0300

    corrigindo cors

--- src/server.js
+++ src/server.js
@@ -9,6 +9,15 @@ import NewsModel from "./module/news/news.model.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 const PORT = process.env.PORT || 8080;
 
