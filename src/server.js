99c9ef053803a44ec52575cb5948526423e2374a
Murillo Alves Couto <murilloacouto@outlook.com>
2024-03-06 19:16:13 -0300

    corrigindo vercel.json

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import NewsModel from "./module/news/news.model.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.options("*", cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
